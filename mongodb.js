7fbafd4cd7618300d1cc92e8a1aa1a1fd452fa21
Bcdirito <bdirito91@gmail.com>
2019-10-02 10:38:14 -0400

    added updated mongo functionality

--- mongodb.js
+++ mongodb.js
@@ -10,29 +10,24 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true, useUnifiedTopology: t
 
     const db = client.db(databaseName)
 
-    // db.collection("users").findOne({_id: new ObjectID("5d935d4812154821ac79f50f")}, (error, user) => {
-    //     if (error) return console.log(error)
-
-    //     console.log(user)
+    // db.collection("users").updateOne({
+    //     __id: new ObjectID("5d935d4812154821ac79f50f")
+    // }, {
+    //     $inc: {
+    //         age: 1
+    //     }
+    // }).then((result) => {
+    //     console.log(result)
+    // }).catch((error) => {
+    //     console.log(error)
     // })
 
-    // db.collection("users").find({age: 27}).toArray((error, users) => {
-    //     if (error) return console.log(error)
-    //     console.log(users)
-    // })
-
-    // db.collection("users").find({age: 27}).count((error, count) => {
-    //     if (error) return console.log(error)
-    //     console.log(count)
-    // })
-
-    db.collection("tasks").findOne({_id: new ObjectID("5d9222761f92a432e8fa8cf8")}, (error, task) => {
-        if (error) return console.log(error)
-        console.log(task)
-    })
-
-    db.collection("tasks").find({completed: false}).toArray((error, tasks) => {
-        if (error) return console.log(error)
-        console.log(tasks)
-    })
+    db.collection("tasks").updateMany({
+        completed: false
+    }, {
+        $set: {
+            completed: true
+        }
+    }).then((result) => console.log(result))
+    .catch((error) => console.log(error))
 })
